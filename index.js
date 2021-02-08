8e926bdcbf96278881abdf5a5a974dfcada1545d
Justin Mattos <justin.mattos@nyu.edu>
2021-02-07 23:18:08 -0500

    refactored with object mutation

--- index.js
+++ index.js
@@ -1,94 +1,75 @@
-const slots = ['first', 'second', 'third'];
-
 const users = [
-  { id: 1, name: 'Justin', slot: 'first' },
-  { id: 2, name: 'Tricia', slot: 'first' },
-  { id: 3, name: 'Atish', slot: 'second' },
-  { id: 4, name: 'Zack', slot: 'second' },
-  { id: 5, name: 'Megan', slot: 'third' },
-  { id: 6, name: 'Laura', slot: 'third' },
-  { id: 7, name: 'Chris', slot: 'first' },
+  { id: 1, name: 'Justin', slot: 'first', selected: false },
+  { id: 2, name: 'Tricia', slot: 'first', selected: false },
+  { id: 3, name: 'Atish', slot: 'second', selected: false },
+  { id: 4, name: 'Zack', slot: 'second', selected: false },
+  { id: 5, name: 'Megan', slot: 'third', selected: false },
+  { id: 6, name: 'Laura', slot: 'third', selected: false },
+  { id: 7, name: 'Chris', slot: 'first', selected: false },
 ];
 
-const first = document.querySelector('#first'),
-  firstUsers = document.createElement('div'),
-  second = document.querySelector('#second'),
-  secondUsers = document.createElement('div'),
-  third = document.querySelector('#third'),
-  thirdUsers = document.createElement('div'),
-  bucket = document.querySelector('#buckets');
+const bucket = document.querySelector('#buckets');
 
-first.appendChild(firstUsers);
-second.appendChild(secondUsers);
-third.appendChild(thirdUsers);
+const refreshLists = () => {
+  const userBuckets = [
+    document.querySelector('#first > div'),
+    document.querySelector('#second > div'),
+    document.querySelector('#third > div'),
+  ];
 
-for (let user of users) {
-  let userNode = document.createElement('h3');
-  userNode.innerHTML = user.name;
-  userNode.id = `user-${user.id}`;
-  switch (user.slot) {
-    case 'first':
-      firstUsers.appendChild(userNode);
-      break;
-    case 'second':
-      secondUsers.appendChild(userNode);
-      break;
-    case 'third':
-      thirdUsers.appendChild(userNode);
+  userBuckets.forEach((bucket) => removeChildren(bucket));
+
+  for (let user of users) {
+    let userNode = document.createElement('h3');
+    userNode.innerHTML = user.name;
+    userNode.id = `user-${user.id}`;
+    if (user.selected) {
+      userNode.classList.toggle('selected');
+    }
+    switch (user.slot) {
+      case 'first':
+        userBuckets[0].appendChild(userNode);
+        break;
+      case 'second':
+        userBuckets[1].appendChild(userNode);
+        break;
+      case 'third':
+        userBuckets[2].appendChild(userNode);
+    }
   }
-}
+};
+
+const removeChildren = (node) => {
+  [...node.childNodes].forEach((child) => {
+    node.removeChild(child);
+  });
+};
 
 const buttonFunc = (target) => {
   let parent = target.parentNode,
-    selected = [...parent.querySelectorAll('.selected')],
+    selectedUsers = users.filter((user) => {
+      return user.slot === parent.id && user.selected;
+    }),
     destination;
+
   if (target.className === 'back') {
-    destination =
-      parent.id === 'second'
-        ? first.querySelector('div')
-        : second.querySelector('div');
-    moveSelectedBack(parent, selected, destination);
+    destination = parent.id === 'second' ? 'first' : 'second';
   } else if (target.className === 'forward') {
-    destination =
-      parent.id === 'second'
-        ? third.querySelector('div')
-        : second.querySelector('div');
-    moveSelectedForward(parent, selected, destination);
+    destination = parent.id === 'second' ? 'third' : 'second';
   }
-  sortUsers(destination);
-};
-
-const moveSelectedBack = (parent, selected, destination) => {
-  if (parent.id === 'first') return;
-  for (let node of selected) {
-    destination.appendChild(node);
-  }
-  parent.childNodes = [...parent.childNodes];
-};
-
-const moveSelectedForward = (parent, selected, destination) => {
-  if (parent.id === 'third') return;
-  for (let node of selected) {
-    destination.appendChild(node);
-  }
+  selectedUsers.forEach((user) => {
+    user.slot = destination;
+  });
+  refreshLists();
 };
 
 const selectFunc = (target) => {
   target.classList.toggle('selected');
+  let id = target.id.slice(5) - 1;
+  users[id].selected = !users[id].selected;
 };
 
-const sortUsers = (destination) => {
-  console.log(destination);
-  [...destination.childNodes]
-    .sort((a, b) => {
-      a = parseInt(a.id.slice(5), 10);
-      b = parseInt(b.id.slice(5), 10);
-      return a - b;
-    })
-    .forEach((node) => {
-      destination.appendChild(node);
-    });
-};
+refreshLists();
 
 bucket.addEventListener('click', (event) => {
   const target = event.target;
